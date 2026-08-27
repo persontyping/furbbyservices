91bbcaa91a14469f2d99a1bdcc77743a24b3f797
Lola <persontyping@pm.me>
2026-08-28 09:21:03 +1000

    carousel adjustments

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -3,7 +3,7 @@ import { Cabin } from './cabin.tsx'
 
 export function Carousel() {
     return (
-        <div className="carousel h-[clamp(32rem,calc(100svh-1rem),56rem)] w-full lg:h-[clamp(38rem,calc(100svh-4rem),56rem)]">
+        <div className="carousel border border-2 h-[clamp(32rem,calc(100svh-1rem),56rem)] w-full lg:h-[clamp(38rem,calc(100svh-4rem),56rem)]">
             <div id="slide1" className="carousel-item relative h-full max-h-full min-h-0 w-full overflow-hidden">
                 <Cabin />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
